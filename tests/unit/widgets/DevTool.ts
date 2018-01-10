1e9b7e4d37fb88b5d8a3a926c1226bb4378703b7
Kitson Kelly <me@kitsonkelly.com>
2018-01-10 14:14:24 +1100

    Restore basic test

--- tests/unit/widgets/DevTool.ts
+++ tests/unit/widgets/DevTool.ts
@@ -2,14 +2,41 @@ const { describe, it } = intern.getInterface('bdd');
 import harness from '@dojo/test-extras/harness';
 import { v, w } from '@dojo/widget-core/d';
 
-import DevTool from '../../../src/widgets/DevTool';
+import DevTool, { DevToolProperties } from '../../../src/widgets/DevTool';
 import * as devtoolCss from '../../../src/widgets/styles/devtool.m.css';
 
 import Button from '@dojo/widgets/button/Button';
 
+function getMockState(): DevToolProperties {
+	return {
+		diagnostics: {
+			eventLog: [],
+			projectors: [],
+			lastRender: null,
+			stores: []
+		},
+		interface: {
+			activeIndex: 0,
+			expandedDNodes: [],
+			expandedProperties: {},
+			expandedStateNodes: []
+		},
+		refreshDiagnostics(): any {
+			return Promise.resolve();
+		},
+		setInterfaceProperty(key: string, value: any): any {
+			return Promise.resolve();
+		},
+		toggleExpanded(key: string, id: string, value: string): any {
+			return Promise.resolve();
+		}
+	};
+}
+
 describe('DevTool', () => {
 	it('should render no diagnostics available by default', () => {
 		const widget = harness(DevTool);
+		widget.setProperties(getMockState());
 		widget.expectRender(
 			v(
 				'div',
@@ -18,20 +45,8 @@ describe('DevTool', () => {
 					key: 'noapi'
 				},
 				[
-					v(
-						'div',
-						{
-							classes: devtoolCss.banner
-						},
-						['No Dojo 2 diagnostics detected']
-					),
-					w(
-						Button,
-						{
-							onClick: widget.listener
-						},
-						['Refresh']
-					)
+					v('div', { classes: devtoolCss.banner }, ['No Dojo 2 diagnostics detected']),
+					w(Button, { onClick: widget.listener }, ['Refresh'])
 				]
 			)
 		);
